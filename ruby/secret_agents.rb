48b060cf8ecefb4da543290fe309757131451783
Jeff Roy <jeffrey_a_roy@yahoo.com>
2016-06-18 19:43:04 -0700

    Added decryption and interface

--- ruby/secret_agents.rb
+++ ruby/secret_agents.rb
@@ -1,11 +1,53 @@
-
+# Methods
 
 def encrypt(string)
 	index = 0
 	while index < string.length
-		string[index] = string[index].next
+		s = string[index]
+		if s == "z"
+			string[index] = "a"
+		elsif s != " "
+			string[index] = string[index].next
+		end
 		index += 1
-	end	
+	end
+	return string
 end
 
-encrypt("abc")
+def decrypt(string)
+	index = 0
+	while index < string.length
+		alpha_reverse = "zyxwvutsrqponmlkjihgfedcba"
+		s = string[index]
+		s_location = alpha_reverse.index(s)
+		if s_location != nil
+			t = alpha_reverse[s_location + 1]
+			if s == "a"
+			string[index] = "z"
+			elsif s != " "
+			string[index] = t
+			end
+		end
+		index += 1
+	end
+	return string
+end
+
+
+#puts decrypt(encrypt("swordfish"))
+
+# Driver Code
+
+print "Would you like to encrypt or decrypt a password? "
+choice = gets.chomp
+
+print "Enter password: "
+password = gets.chomp
+
+if choice == "encrypt"
+	puts "The encryption is: " + encrypt(password)
+elsif choice == "decrypt"
+	puts "The decryption is: " + decrypt(password)
+else
+	puts "I didn't recognize your choice"
+end
