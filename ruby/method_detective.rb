75e1323fe7efb71c254d19de8443a838b8e42cfb
Jeff Roy <jeffrey_a_roy@yahoo.com>
2016-06-22 19:59:48 -0700

    Completed exercise 5.1

--- ruby/method_detective.rb
+++ ruby/method_detective.rb
@@ -5,21 +5,21 @@
 # committed the crime, add additional calls to prove it.
 
 # WARNING--
-# The notorious criminals ".replace" and ".sub" could have committed every
-# crime in this list!  Recommend immediate apprehension.
+# The notorious criminals ".replace" and ".sub" could have committed almost
+# every crime in this list!  Recommend immediate apprehension.
 # Here is an example of their versatility:
 
-p "same".replace("different")
-p "same".sub("same", "different")
-p "same".gsub("same", "different")
+puts "same".replace("different")
+puts "same".sub("same", "different")
+puts "same".gsub("same", "different")
 
 # I'll omit these shapeshifters from the rest of my analysis.
 
-p "iNvEsTiGaTiOn".swapcase
+puts "iNvEsTiGaTiOn".swapcase
 
 # => “InVeStIgAtIoN”
 
-p "zom".insert(1, "o")
+puts "zom".insert(1, "o")
 # => “zoom”
 
 p "enhance".center(15)
@@ -28,26 +28,40 @@ p "enhance".center(15)
 puts "Stop! You’re under arrest!".upcase
 # => "STOP! YOU’RE UNDER ARREST!"
 
-p "the usual".concat("suspects")
-p "the usual".insert(9, "suspects")
-
+puts "the usual".concat(" suspects")
+puts "the usual".insert(9, " suspects")
 #=> "the usual suspects"
 
-# " suspects".<???>
+puts " suspects".prepend("the usual")
+puts " suspects".insert(0, "the usual")
 # => "the usual suspects"
 
-# "The case of the disappearing last letter".<???>
+puts "The case of the disappearing last letter".chop
+puts "The case of the disappearing last letter".chomp("r")
+puts "The case of the disappearing last letter".slice(0, 39)
+puts "The case of the disappearing last letter".slice(0..38)
+puts "The case of the disappearing last letter".slice("The case of the disappearing last lette")
+puts "The case of the disappearing last letter".byteslice(0, 39)
+puts "The case of the disappearing last letter".byteslice(0..38)
 # => "The case of the disappearing last lette"
 
-# "The mystery of the missing first letter".<???>
+puts "The mystery of the missing first letter".delete("T")
+puts "The mystery of the missing first letter".slice(1, 40)
+puts "The mystery of the missing first letter".slice(1..39)
+puts "The mystery of the missing first letter".slice("he mystery of the missing first letter")
+puts "The mystery of the missing first letter".byteslice(1, 40)
+puts "The mystery of the missing first letter".byteslice(1..39)
+puts "The mystery of the missing first letter".sub("T", "")
+puts "The mystery of the missing first letter".gsub("T", "")
 # => "he mystery of the missing first letter"
 
-# "Elementary,    my   dear        Watson!".<???>
+puts "Elementary,    my   dear        Watson!".squeeze(" ")
 # => "Elementary, my dear Watson!"
 
-# "z".<???>
+puts "z".ord
 # => 122 
 # (What is the significance of the number 122 in relation to the character z?)
+# 122 is the ASCII code for lowercase "z"
 
-# "How many times does the letter 'a' appear in this string?".<???>
+puts "How many times does the letter 'a' appear in this string?".count("a")
 # => 4
