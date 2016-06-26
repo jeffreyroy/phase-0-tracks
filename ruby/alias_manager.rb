702a3c5ed42424c3f223c1c2cbbe236ea7cb9f9a
Jeff Roy <jeffrey_a_roy@yahoo.com>
2016-06-25 21:57:28 -0700

    Add pseudocode for alias generator

---
+++ ruby/alias_manager.rb
@@ -0,0 +1,27 @@
+
+
+## Define method to swap first and last names
+	# convert name into array
+	# l = length of array - 1
+	# temp = array[0]
+	# array[0] = array [l]
+	# array[l] = temp
+	# convert array back into new string
+	# return new string
+
+## Define method to switch vowels and consonants
+	## Create master replacement list
+	## We'll replace each letter with the next one in the list
+	# create master list of letters
+	# swap_list = "aeiouabcdfghjklmnpqrstvwxyzb"
+	# swap_list = swap_list + swap_list.upcase
+	# for each letter in name
+	# => i = index of letter in master list
+	# => if i not nil
+	# =>  	switch letter with next letter in list
+	# => end if
+
+# Get name as string
+# Call name swapping method
+# Call letter swapping method
+# Print new alias
