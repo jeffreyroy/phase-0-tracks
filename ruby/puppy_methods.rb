ba78ae831b74d1ee746232530d8e44307f8aa0eb
Jeff Roy <jeffrey_a_roy@yahoo.com>
2016-07-10 18:34:25 -0700

    complete release 0

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -1,9 +1,29 @@
+# Puppy class
 class Puppy
 
   def fetch(toy)
     puts "I brought back the #{toy}!"
     toy
   end
-
+  def speak(n)
+  	puts "Woof! " * n
+  end
+  def roll_over
+  	puts "*rolls over*"
+  end
+  def dog_years(n)
+  	n * 7
+  end
+  def chase_tail
+  	puts "*runs in a circle, chasing his tail*"
+  end
 end
 
+# Driver code
+fido = Puppy.new
+
+fido.fetch("ball")
+fido.speak(3)
+fido.roll_over
+puts "11 human years is #{fido.dog_years(11)} dog years."
+fido.chase_tail
