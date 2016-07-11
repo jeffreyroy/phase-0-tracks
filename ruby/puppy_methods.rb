02f90326c7ee2324317fe5d36a0e1169486cc9de
Jeff Roy <jeffrey_a_roy@yahoo.com>
2016-07-10 18:47:09 -0700

    complete release 2

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -1,6 +1,9 @@
 # Puppy class
 class Puppy
 
+  def initialize
+  	puts "Initializing a new Puppy instance..."
+  end
   def fetch(toy)
     puts "I brought back the #{toy}!"
     toy
@@ -19,11 +22,39 @@ class Puppy
   end
 end
 
+# Kitten class
+class Kitten
+  def initialize
+  	  puts "Creating a kitten..."
+  end
+  def meow
+  	puts "Meow!"
+  end
+  def catch(rodent)
+  	puts "The kitten caught a #{rodent}!"
+  end
+end
+
 # Driver code
 fido = Puppy.new
-
 fido.fetch("ball")
 fido.speak(3)
 fido.roll_over
 puts "11 human years is #{fido.dog_years(11)} dog years."
 fido.chase_tail
+
+felix = Kitten.new
+felix.meow
+felix.catch("mouse")
+
+# Create 50 kittens
+kitten_list=[]
+for count in 1..50
+	kitten_list << Kitten.new
+end
+
+# Loop over 50 kittens
+kitten_list.each do |a_kitten|
+	a_kitten.meow
+	a_kitten.catch("rat")
+end
