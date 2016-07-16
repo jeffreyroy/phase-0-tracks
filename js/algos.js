d57ba411eb36b6869fa066a6cc4887e4079900d6
Jeff Roy <jeffrey_a_roy@yahoo.com>
2016-07-15 18:32:55 -0700

    complete release 2

--- js/algos.js
+++ js/algos.js
@@ -22,7 +22,7 @@ function longest(stringArray) {
 	} 
 	// Loop through array, checking to see if current element is longest so far
 	var result = "";
-	for (i=0; i<l; i++) {
+	for (var i=0; i<l; i++) {
 		if (stringArray[i].length > result.length) {
 			result = stringArray[i];
 		}
@@ -48,16 +48,63 @@ end of function
 function keyMatch(obj1, obj2) {
 	// The keys() method seems like it should work, but it doesn't for some reason
 	// So we'll use the in operator instead
+	// Loop through keys in both objects to see if we find a key match
 	for(var property in obj1) {
 		if(property in obj2) {
-			console.log("Found a key match: " + property)
+
+			// If keys match, check for value match
 			if(obj1[property] == obj2[property]) {
-				console.log("Found a value match: " + obj1[property]);
+
 				return true;
 			}
 		}
 	}
+	// If no match, return false
 	return false;
+}
+
+/* Generate random list of words
+pseudocode
+
+function wordArray(n)
+	initialize empty array
+	loop n times
+		initialize empty string
+		select random number l between 1 and 10
+		loop l times
+			select random number between 1 and 26
+			find corresponding letter of alphabet
+			add that letter to string
+		end of loop
+		add string to array
+	end loop
+	return array
+end of function
+*/
+
+// randomInt(n) returns a random integer between 1 and n
+function randomInt(n) {
+	return Math.floor(Math.random() * n) + 1;
+}
+
+// randomWord generates a gibberish "word" of 1-10 random letters
+function randomWord() {
+	var alphabet = "abcdefghijklmnopqrstuvwxyz";
+	var l = randomInt(10);
+	var word = "";
+	for(var i=1; i<=l; i++) {
+		word = word + alphabet.charAt(randomInt(26) - 1);
+	}
+	return word;
+}
+
+// wordArrary(n) returns array of n random words
+function wordArray(n) {
+	var array = [];
+	for(var i=1; i<=n; i++) {
+		array.push(randomWord());
+	}
+	return array;
 }
 
 // Driver code
@@ -68,14 +115,25 @@ console.log(longest([]));
 var johnDoe = {
 	firstName: "John",
 	middleName: "A",
-	lastName: "Doe"
+	lastName: "Doe",
+	age: 54
 };
 
 var richardRoe = {
 	firstName: "Richard",
-	lastName: "Roe"
+	lastName: "Roe",
+	age: 54
 };
 
-console.log(johnDoe["firstName"]);
 console.log(keyMatch(johnDoe, richardRoe));
 
+johnDoe.age = 55;
+console.log(keyMatch(johnDoe, richardRoe));
+
+
+
+var testArray
+for(var i=1; i<=10; i++) {
+	testArray = wordArray(3);
+	console.log("The longest of " + testArray + " is " + longest(testArray) + ".");
+}
