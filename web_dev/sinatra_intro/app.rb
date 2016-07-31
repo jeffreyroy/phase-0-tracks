29cb81d94ef6a78947696d05169bd5601adc9b74
Jeff Roy <jeffrey_a_roy@yahoo.com>
2016-07-30 20:09:35 -0700

    add subpages and search feature to app

--- web_dev/sinatra_intro/app.rb
+++ web_dev/sinatra_intro/app.rb
@@ -50,3 +50,51 @@ get '/students/:id' do
     response << "Campus: #{student['campus']}<br><br>"
   response
 end
+
+get '/contact' do
+  "Sherlock Holmes<br>
+  221b Baker Street<br>
+  London NW1 6XE<br>
+  United Kingdom<br>"
+end
+
+get '/great_job' do
+  name = params[:name]
+  response = "Good job"
+  if name
+    response << ", #{name}"
+  end
+  response << "!"
+  response
+end
+
+get '/add/:x/:y' do
+  x = params[:x].to_i
+  y = params[:y].to_i
+  "#{x} + #{y} = #{x+y}"
+end
+
+get '/search' do
+  "<form action='result'>
+  <p>Search for a student by name:</p>
+  <p>Full name:
+  <input type='text' name='name' />
+  </p>"
+end
+
+get '/result' do
+  name=params[:name]
+  students = db.execute("SELECT * FROM students WHERE name=?", name)
+  if students.empty?
+    "No students found with that name."
+  else
+    response = ""
+    students.each do |student|
+      response << "ID: #{student['id']}<br>"
+      response << "Name: #{student['name']}<br>"
+      response << "Age: #{student['age']}<br>"
+      response << "Campus: #{student['campus']}<br><br>"
+    end
+    response
+  end
+end
