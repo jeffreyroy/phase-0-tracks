867825c8c864c8e7615659fd5d80a19dd4437a0c
Jeff Roy <jeffrey_a_roy@yahoo.com>
2016-07-31 20:14:13 -0700

    allow addition of new campuses and format web pages

--- web_dev/sinatra_templating/app.rb
+++ web_dev/sinatra_templating/app.rb
@@ -25,13 +25,24 @@ post '/students' do
   redirect '/'
 end
 
+# Search for students by campus using list of campuses in database
 get '/campuses/search' do
 	@campuses = db.execute("SELECT * FROM campuses")
 	erb :campus_search
 end
 
+# Add a new campus
+	# (Can add more functionality, e.g. check for duplicates)
+post '/campuses/search' do
+  db.execute("INSERT INTO campuses (city, abbreviation) VALUES (?,?)", [params['city'].capitalize, params['abbreviation'].upcase])
+	redirect '/campuses/search'
+end
+
+# Print list of students at specific campus
 get '/campuses' do
 	campus = params[:campus]
+	@campus_name = db.execute("SELECT city FROM campuses WHERE abbreviation=?", [campus])
+	p @campus_name
 	@students = db.execute("SELECT * FROM students WHERE campus=?", [campus])
 
 	erb :campuses
